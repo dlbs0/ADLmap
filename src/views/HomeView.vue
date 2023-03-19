0d3af30c4388034c857c1f840f5745cbe9762f05
Daniel Birch-Smith <daniel.birch-smith@sageautomation.com>
2023-03-19 23:01:10 +1030

    basic working map

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,44 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import mapboxgl from 'mapbox-gl';
+import { onMounted } from 'vue';
+import { mapData } from './data';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGxicyIsImEiOiJjazQ5MW05NnYwMGp5M2ZwZGhlaXlrMjZoIn0.Mftbu2WiRY4SChlAU8mfrA';
+
+onMounted(() => {
+  console.log('loading map');
+  const mapbox = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [138.5311, -34.9462],
+    zoom: 10
+    // pitch: 30,
+    // bearing: -83,
+  });
+  mapbox.on('style.load', () => {
+    mapbox.addSource('areas', { type: 'geojson', data: mapData });
+    mapbox.addLayer({
+      id: 'areas',
+      type: 'fill',
+      source: 'areas',
+      paint: {
+        'fill-color': '#088',
+        'fill-opacity': 0.8
+      }
+    });
+  });
+});
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <link href="https://api.tiles.mapbox.com/mapbox-gl-js/v2.13.0/mapbox-gl.css" rel="stylesheet" />
+
+  <div id="map" class="map"></div>
 </template>
+
+<style scoped>
+.map {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
