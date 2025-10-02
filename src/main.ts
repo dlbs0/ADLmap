cd0f65ca6cd96d1f2414acc559260cd1e7711dcb
PERMOBIL\U-DAN-0040 <daniel.birch-smith@permobil.com>
2025-10-02 04:39:38 +0200

    working multiplayer

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,37 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import App from './App.vue';
+import router from './router';
 
-import './assets/main.css'
+import './assets/main.css';
 
-const app = createApp(App)
+import { initializeApp } from 'firebase/app';
+import { VueFire, VueFireAuth } from 'vuefire';
 
-app.use(createPinia())
-app.use(router)
+import { getFirestore, collection } from 'firebase/firestore';
+// ... other firebase imports
+const firebaseConfig = {
+  apiKey: 'AIzaSyDlLdW6Ce7zP4QJwWTnqZ710psL4DiMSU0',
+  authDomain: 'adljetlagmap.firebaseapp.com',
+  projectId: 'adljetlagmap',
+  storageBucket: 'adljetlagmap.firebasestorage.app',
+  messagingSenderId: '1069443483517',
+  appId: '1:1069443483517:web:f594952b90c7df8c042e14'
+};
 
-app.mount('#app')
+export const firebaseApp = initializeApp(firebaseConfig);
+
+// used for the firestore refs
+const db = getFirestore(firebaseApp);
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(VueFire, {
+  firebaseApp,
+  modules: []
+});
+app.use(router);
+
+app.mount('#app');
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
 import mapboxgl from 'mapbox-gl';
-import { onMounted, ref, watch } from 'vue';
+import { computed, onMounted, ref, watch } from 'vue';
 import { gameState as gameStateInitial, mapData } from './data';
+import { area } from '@turf/turf';
+import { useDocument } from 'vuefire';
+import { collection, doc, getFirestore, setDoc } from 'firebase/firestore';
+import { firebaseApp } from '@/main';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGxicyIsImEiOiJjazQ5MW05NnYwMGp5M2ZwZGhlaXlrMjZoIn0.Mftbu2WiRY4SChlAU8mfrA';
 
 const currentlySelectedState = ref<string | null>(null);
 
-const gameState = ref(gameStateInitial);
+// const gameState = ref(gameStateInitial);
 
 let map: mapboxgl.Map | null = null;
+
+const db = getFirestore(firebaseApp);
+const gameState = useDocument(doc(collection(db, 'gameState'), 'game1'));
+
+// setDoc(doc(db, 'gameState', 'game1'), gameState.value, { merge: false });
 
 onMounted(() => {
   console.log('loading map');
@@ -30,7 +39,7 @@ onMounted(() => {
       source: 'areas',
       paint: {
         'fill-color': ['get', 'fillColor'],
-        'fill-opacity': ['case', ['==', ['get', 'fillColor'], '#088'], 0.1, 0.5]
+        'fill-opacity': ['case', ['!=', ['get', 'fillColor'], '#088'], 0.5, 0.1]
       }
     });
     map.addLayer({
@@ -42,10 +51,16 @@ onMounted(() => {
         'line-opacity': 1
       }
     });
+    updateMap();
     map.on('click', 'areas', (e) => {
       if (!e.lngLat || !e.features) return;
       if (!map) return;
-      new mapboxgl.Popup().setLngLat(e.lngLat).setHTML(e?.features[0].properties?.electorate).addTo(map);
+      const areaResult = area(e.features[0]) * 0.000001;
+      console.log('areaResult', areaResult);
+      new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML(`${e?.features[0].properties?.electorate} <br/> ${areaResult.toFixed(1)}km2`)
+        .addTo(map);
       currentlySelectedState.value = e?.features[0].properties?.electorate;
     });
   });
@@ -64,6 +79,10 @@ watch(gameState, (newState) => {
   console.log('gameState changed', newState);
   // Update map colors based on game state
   // Iterate through the mapdata features and set the fill color based on the game state
+  updateMap();
+});
+
+function updateMap(newState = gameState.value) {
   if (!map) return;
   const colors: Record<string, string> = {
     unclaimed: '#088',
@@ -85,7 +104,7 @@ watch(gameState, (newState) => {
     type: 'FeatureCollection',
     features
   });
-});
+}
 
 function claim(claimer: '1' | '2' | 'unclaimed') {
   if (!currentlySelectedState.value) return;
@@ -93,18 +112,54 @@ function claim(claimer: '1' | '2' | 'unclaimed') {
     ...gameState.value,
     [currentlySelectedState.value]: claimer
   };
+  const key1 = currentlySelectedState.value;
+  // use the firesotre api to set the game state
+  setDoc(doc(db, 'gameState', 'game1'), { [key1]: claimer }, { merge: true });
 }
+
+const score = computed(() => {
+  const teams: Record<string, { count: number; area: number }> = {
+    '1': { count: 0, area: 0 },
+    '2': { count: 0, area: 0 },
+    unclaimed: { count: 0, area: 0 }
+  };
+  if (!gameState.value) return teams;
+  Object.keys(gameState.value).forEach((k) => {
+    if (!gameState.value) return;
+    const v = gameState.value[k as keyof typeof gameState.value] || 'unclaimed';
+    teams[v].count = teams[v].count + 1;
+    const feature = mapData.features.find((f) => f.properties?.electorate === k);
+    console.log('feature for', k, feature);
+    if (feature) {
+      teams[v].area = teams[v].area + (area(feature) * 0.000001 || 0);
+    }
+  });
+  return teams;
+});
 </script>
 
 <template>
   <link href="https://api.tiles.mapbox.com/mapbox-gl-js/v3.15.0/mapbox-gl.css" rel="stylesheet" />
 
   <div id="map" class="map"></div>
-  <div class="buttonHolder">
-    {{ currentlySelectedState }}
-    <button @click="claim('1')">Claim 1</button>
-    <button @click="claim('2')">Claim 2</button>
-    <button @click="claim('unclaimed')">Clear</button>
+  <div class="center">
+    <div class="scoreHolder">
+      <div class="team" v-for="(value, key) in score" :key="value">
+        <h2>{{ key != 'unclaimed' ? 'Team ' + key : key }}</h2>
+        <!-- {{ value }} -->
+        Count: {{ value.count }} <br />
+        Area: {{ value.area.toFixed(1) }}km²
+      </div>
+      <!-- {{ score }} -->
+    </div>
+  </div>
+  <div class="center">
+    <div class="buttonHolder">
+      {{ currentlySelectedState }}
+      <button @click="claim('1')">Claim 1</button>
+      <button @click="claim('2')">Claim 2</button>
+      <button @click="claim('unclaimed')">Clear</button>
+    </div>
   </div>
 </template>
 
@@ -113,12 +168,18 @@ function claim(claimer: '1' | '2' | 'unclaimed') {
   width: 100vw;
   height: 100vh;
 }
+.center {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  /* pointer-events: none; Allow clicks to pass through */
+}
 .buttonHolder {
   position: absolute;
   bottom: 10px;
   /* left: 10px; */
   z-index: 1;
-  width: 100%;
+  /* width: 100%; */
   display: flex;
   justify-content: center;
   gap: 8px;
@@ -126,5 +187,21 @@ function claim(claimer: '1' | '2' | 'unclaimed') {
   padding: 8px;
   border-radius: 4px;
   margin: auto;
+}
+
+.scoreHolder {
+  position: absolute;
+  text-align: center;
+  top: 10px;
+  z-index: 1;
+  background-color: rgba(255, 255, 255, 0.8);
+  padding: 8px;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  gap: 16px;
+  h2 {
+    margin: 0;
+  }
 }
 </style>
