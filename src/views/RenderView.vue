5e0d9a1cf1c090e9ac69873108d04e2e9db97d20
Daniel Birch-Smith <dlbs@outlook.com>
2025-10-19 13:50:32 +0200

    style updates

--- src/views/RenderView.vue
+++ src/views/RenderView.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import mapboxgl from 'mapbox-gl';
 import { computed, onMounted, ref, watch } from 'vue';
 import { gameState as gameStateInitial, mapData } from './data';
@@ -6,27 +6,46 @@ import { area } from '@turf/turf';
 import { useDocument } from 'vuefire';
 import { collection, doc, getFirestore, setDoc } from 'firebase/firestore';
 import { firebaseApp } from '@/main';
+// import { loadEncoder } from 'h264-mp4-encoder';
+import * as HME from 'h264-mp4-encoder';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZGxicyIsImEiOiJjazQ5MW05NnYwMGp5M2ZwZGhlaXlrMjZoIn0.Mftbu2WiRY4SChlAU8mfrA';
+import * as wasmFeatureDetect from 'wasm-feature-detect';
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGxiczAiLCJhIjoiY20wdGlpMmc2MHJqaDJsczVtNXRvN2ZneCJ9.47aVkXUGN8JNldnZUjj-nA';
 
 const currentlySelectedState = ref<string | null>(null);
 
 // const gameState = ref(gameStateInitial);
-const colours: Record<string, { colour: string; teamName: string; colourName: string }> = {
-  unclaimed: { colour: '#088', teamName: 'Unclaimed', colourName: 'Clear' },
-  1: { colour: '#9b3531', teamName: 'Red Team', colourName: 'Red' },
-  2: { colour: '#0d546b ', teamName: 'Blue Team', colourName: 'Blue' }
-};
 
 let map: mapboxgl.Map | null = null;
 
 const db = getFirestore(firebaseApp);
 const gameState = useDocument(doc(collection(db, 'gameState'), 'game1'));
+const supportsSIMD = wasmFeatureDetect.simd();
+// const supportsSIMD = await simd();
+const encoder = await HME.createH264MP4Encoder();
+// initialize H264 video encoder
+// const Encoder = await loadEncoder({ simd: supportsSIMD });
+
+// stub performance.now for deterministic rendering per-frame (only available in dev build)
+
+// const ptr = encoder.getRGBPointer(); // keep a pointer to encoder WebAssembly heap memory
 
 // setDoc(doc(db, 'gameState', 'game1'), gameState.value, { merge: false });
 
 onMounted(() => {
+  function frame() {
+    // increment stub time by 16.6ms (60 fps)
+    now += 1000 / 60;
+    if (!map) return;
+    mapboxgl?.setNow(now);
+
+    const pixels = encoder.memory().subarray(ptr); // get a view into encoder memory
+
+    gl.readPixels(0, 0, width, height, gl.RGBA, gl.UNSIGNED_BYTE, pixels); // read pixels into encoder
+
+    encoder.encodeRGBPointer(); // encode the frame
+  }
   console.log('loading map');
   map = new mapboxgl.Map({
     container: 'map',
@@ -37,6 +56,19 @@ onMounted(() => {
     antialias: true
   });
   if (!map) return;
+  const gl = map.painter.context.gl;
+  const width = gl.drawingBufferWidth;
+  const height = gl.drawingBufferHeight;
+
+  encoder.width = width;
+  encoder.height = height;
+  encoder.frameRate = 60;
+  encoder.kbps = 64000;
+
+  let now = performance.now();
+  mapboxgl.setNow(now);
+
+  map.on('render', frame); // set up frame-by-frame recording
   map.on('style.load', () => {
     if (!map) return;
 
@@ -89,12 +121,9 @@ onMounted(() => {
         'text-halo-width': 2
       }
     });
-    // setTimeout(() => {
-    //   updateMap();
-    // }, 2000);
-    gameState.promise.value.then(() => {
+    setTimeout(() => {
       updateMap();
-    });
+    }, 2000);
     map.on('click', 'areas', (e) => {
       if (!e.lngLat || !e.features) return;
       if (!map) return;
@@ -117,21 +146,26 @@ onMounted(() => {
   );
 });
 
-watch(gameState, () => {
+watch(gameState, (newState) => {
   // Update map colors based on game state
+  // Iterate through the mapdata features and set the fill color based on the game state
   updateMap();
 });
 
 function updateMap(newState = gameState.value) {
   if (!map) return;
-
+  const colors: Record<string, string> = {
+    unclaimed: '#088',
+    1: '#9b3531',
+    2: '#0d546b '
+  };
   const features = mapData.features.map((feature) => {
     const electorate = feature.properties?.electorate;
     //@ts-expect-error electorate is always defined
     const state = newState[electorate] || 'unclaimed';
     feature.properties = {
       ...feature.properties,
-      fillColor: colours[state].colour
+      fillColor: colors[state]
     };
     return feature;
   });
@@ -149,26 +183,24 @@ function claim(claimer: '1' | '2' | 'unclaimed') {
     [currentlySelectedState.value]: claimer
   };
   const key1 = currentlySelectedState.value;
-  // use the firestore api to set the game state
+  // use the firesotre api to set the game state
   setDoc(doc(db, 'gameState', 'game1'), { [key1]: claimer }, { merge: true });
 }
 
 const score = computed(() => {
-  let teams: Record<string, { count: number; area: number; teamName: string; colourName: string; colour: string }> = Object.keys(
-    colours
-  ).reduce((acc, k) => {
-    acc[k] = { count: 0, area: 0, teamName: colours[k].teamName, colourName: colours[k].colourName, colour: colours[k].colour };
-    return acc;
-  }, {} as Record<string, { count: number; area: number; teamName: string; colourName: string; colour: string }>);
-
+  const teams: Record<string, { count: number; area: number }> = {
+    '1': { count: 0, area: 0 },
+    '2': { count: 0, area: 0 },
+    unclaimed: { count: 0, area: 0 }
+  };
   if (!gameState.value) return teams;
   Object.keys(gameState.value).forEach((k) => {
     if (!gameState.value) return;
     const v = gameState.value[k as keyof typeof gameState.value] || 'unclaimed';
-    teams[v].count = (teams[v].count ?? 0) + 1;
+    teams[v].count = teams[v].count + 1;
     const feature = mapData.features.find((f) => f.properties?.electorate === k);
     if (feature) {
-      teams[v].area = (teams[v].area ?? 0) + (area(feature) * 0.000001 || 0);
+      teams[v].area = teams[v].area + (area(feature) * 0.000001 || 0);
     }
   });
   return teams;
@@ -182,11 +214,9 @@ const score = computed(() => {
   <div class="center">
     <div class="scoreHolder">
       <div class="team" v-for="(value, key) in score" :key="key">
-        <h3 :style="{ color: value.colour }">{{ value.teamName }}</h3>
-        <div class="count" :style="{ color: value.colour }">
-          {{ value.count }}
-        </div>
-        {{ value.area?.toFixed(1) }}km²
+        <h2>{{ key != 'unclaimed' ? 'Team ' + key : key }}</h2>
+        Count: {{ value.count }} <br />
+        Area: {{ value.area.toFixed(1) }}km²
       </div>
     </div>
   </div>
@@ -195,9 +225,9 @@ const score = computed(() => {
       <div class="currSelected">
         {{ currentlySelectedState }}
       </div>
-      <template v-for="(value, key) in colours" :key="key">
-        <button @click="claim(key as '1' | '2' | 'unclaimed')">{{ value.colourName }}</button>
-      </template>
+      <button @click="claim('1')">CLAIM 1</button>
+      <button @click="claim('2')">CLAIM 2</button>
+      <button @click="claim('unclaimed')">CLEAR</button>
     </div>
   </div>
 </template>
@@ -247,21 +277,10 @@ const score = computed(() => {
   border-radius: 4px;
   display: flex;
   justify-content: center;
-  gap: 32px;
+  gap: 16px;
   h2 {
     margin: 0;
   }
-  h3 {
-    margin: 0;
-  }
-}
-.team {
-  /* opacity: 0.8; */
-  background-color: #fafafa44;
-}
-.count {
-  font-size: 2em;
-  font-weight: bold;
 }
 
 button {
@@ -274,4 +293,4 @@ button {
   color: white;
   transition: background-color 0.3s ease;
 }
-</style>
+</style> -->
